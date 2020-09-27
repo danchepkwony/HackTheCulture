4fae6d70c1356d909c34699fa99d22434ec4a00b
Chris J. V <49849059+Jvalery245@users.noreply.github.com>
2020-09-27 18:07:22 -0500

    WPF Desktop - Application
    Adding proof-of-concept for database relationships and database exchanges.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using Spire.Barcode;
+
+namespace Comm_App_Beta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Get_AWS_Data_Click(object sender, RoutedEventArgs e)
+        {
+            string ID_Code = "";
+            string Key_return = "";
+            Communication_DatabaseEntities1 var_data = new Communication_DatabaseEntities1();
+            var Key_Return = var_data.Register_User(First_Name_Input.Text, Last_Name_Input.Text);
+            foreach (var item in Key_Return)
+            {
+                Console.WriteLine(item.Pass_Key);
+                Console.WriteLine(item.User_ID);
+                ID_Code = item.User_ID.ToString();
+                Key_return = item.Pass_Key;
+
+            }
+            string QR_INPUT = ID_Code + "," + Key_return;
+            BarcodeSettings bs = new BarcodeSettings();
+            bs.Type = BarCodeType.QRCode;
+            bs.Data = QR_INPUT;
+            BarCodeGenerator bg = new BarCodeGenerator(bs);
+            bg.GenerateImage().Save("QRCode.png");
+            System.Diagnostics.Process.Start("QRCode.png");
+            MessageBox.Show("User Added!");
+            Outside_User enter_pass = new Outside_User();
+            enter_pass.Show();
+        }
+
+        private void OutSide_Access_Click(object sender, RoutedEventArgs e)
+        {
+            Outside_User enter_pass = new Outside_User();
+            enter_pass.Show();
+        }
+    }
+}
---
+++ Outside_User.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Spire.Barcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Comm_App_Beta
+{
+    /// <summary>
+    /// Interaction logic for Outside_User.xaml
+    /// </summary>
+    public partial class Outside_User : Window
+    {
+        public Outside_User()
+        {
+            InitializeComponent();
+            
+            //var_data.Read_Message(valuetopass.Split(","))
+        }
+
+        private void Read_Mess_Click(object sender, RoutedEventArgs e)
+        {
+            Communication_DatabaseEntities1 var_data = new Communication_DatabaseEntities1();
+            string valuetopass = "";
+            string[] reading_image = BarcodeScanner.Scan("QRCode.png", Spire.Barcode.BarCodeType.QRCode);
+            if (reading_image != null)
+            {
+                foreach (var item in reading_image)
+                {
+                    Console.WriteLine("Credentials found. Logging into system.");
+                    valuetopass = item;
+                }
+            }
+            Console.WriteLine(valuetopass);
+            var call_data = valuetopass.Split(',');
+            Console.WriteLine(call_data[0]);
+            var call_results = var_data.Read_Message(Int32.Parse(call_data[0]));
+            string final_text = "";
+            foreach (var item in call_results)
+            {
+                if (string.IsNullOrEmpty(item.Messages))
+                {
+                    Message_Box_One.Text = "No new messages";
+                }
+                else
+                {
+                    Console.WriteLine(item.Time_Stamp.ToString());
+                    final_text = final_text + item.Time_Stamp.ToString() + "  -->  " + item.Messages.ToString() +" \n";
+                }
+            }
+            Message_Box_One.Text = final_text;
+
+
+        }
+
+        private void Sen_Message_Click(object sender, RoutedEventArgs e)
+        {
+            Communication_DatabaseEntities1 var_data = new Communication_DatabaseEntities1();
+            string valuetopass = "";
+            string[] reading_image = BarcodeScanner.Scan("QRCode.png", Spire.Barcode.BarCodeType.QRCode);
+            if (reading_image != null)
+            {
+                foreach (var item in reading_image)
+                {
+                    Console.WriteLine("Credentials found. Logging into system.");
+                    valuetopass = item;
+                }
+            }
+            Console.WriteLine(valuetopass);
+            var call_data = valuetopass.Split(',');
+            Console.WriteLine(call_data[0]);
+            var_data.New_Message(Int32.Parse(call_data[0]), Text_Outbound.Text);
+        }
+    }
+}
